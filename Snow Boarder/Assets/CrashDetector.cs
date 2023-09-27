cd6bd42aa6aff3e7ab5ac55c326d6bbf4a4e27b4
FarVillage117 <aidanlongueville117@gmail.com>
2023-09-27 12:51:00 -0400

    SceneManagement

--- Snow Boarder/Assets/CrashDetector.cs
+++ Snow Boarder/Assets/CrashDetector.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("Ouch");
+            SceneManager.LoadScene(0);
         }
     }
 }
