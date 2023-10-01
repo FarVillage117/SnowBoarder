139ea2fc523a15e8544f0106358b613afa21a521
FarVillage117 <aidanlongueville117@gmail.com>
2023-10-01 13:55:47 -0400

    PublicAccess

--- Snow Boarder/Assets/PlayerController.cs
+++ Snow Boarder/Assets/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
 
     Rigidbody2D rb2d;
     SurfaceEffector2D surfaceEffector2D;
+    bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +23,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-        RespondToBoost();
+        if (canMove)
+        {
+            RotatePlayer();
+            RespondToBoost();
+        }
     }
 
+    public void DisableControls()
+    {
+        canMove = false;
+    }
     void RespondToBoost()
     {
         if (Input.GetKey(KeyCode.UpArrow))
